09e3840dcb8fe383984ebd23cf7a6e88a1a8fc13
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-01 20:46:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,16 +1,16 @@
 #introduction
 def introduction(name)
-  put "Hi, my name is #{name}."
+  "Hi, my name is #{name}."
 end
 
 #introduction_with_language
 def introduction_with_language(name, language)
-  put "Hi, my name is #{name} and I am learning to program in #{language}."
+  "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 #introduction_with_language_optional
 def introduction_with_language_optional(name, language = "Ruby")
-  put "Hi, my name is #{name} and I am learning to program in #{language}."
+  "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
