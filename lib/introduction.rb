1dc37e59ca4e9296884d81651be072643f8a9fd4
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-01 20:41:16 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+#introduction
+def introduction(name)
+  put "Hi, my name is #{name}."
+end
+
+#introduction_with_language
+def introduction_with_language(name, language)
+  put "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+
